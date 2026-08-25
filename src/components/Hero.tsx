4d01ce434b4e4b98f1e0e2b7f8099ab822939f2e
kaitlync31 <kaitlynchen2028@u.northwestern.edu>
2026-08-24 21:23:58 -0700

    Update hero supporting copy with school and refined interests

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,8 +8,9 @@ export default function Hero() {
         Bringing the vision to life.
       </h1>
       <p className="mt-7 max-w-lg font-sans text-lg leading-8 text-stone">
-        I&apos;m a computer science + cognitive science student exploring the
-        intersection of technology, human behavior, and product.
+        I&apos;m a computer science + cognitive science student at
+        Northwestern University fascinated by the intersections of people,
+        information, and technology.
       </p>
       <a
         href="#collage"
