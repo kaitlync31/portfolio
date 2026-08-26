3cd37320140d6d969a46e07ec919cc7b0ff9cd31
kaitlync31 <kaitlynchen2028@u.northwestern.edu>
2026-08-25 23:35:27 -0700

    Remove Collage section from homepage for now (component kept for later)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import Collage from "@/components/Collage";
 import Intro from "@/components/Intro";
 import Work from "@/components/Work";
 import Leadership from "@/components/Leadership";
@@ -14,7 +13,6 @@ export default function Home() {
       <Navbar />
       <main className="flex-1">
         <Hero />
-        <Collage />
         <Intro />
         <Work />
         <Leadership />
