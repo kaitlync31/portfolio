03ee039da7e8fb08b88525dd8cfbce54bb6025fc
kaitlync31 <kaitlynchen2028@u.northwestern.edu>
2026-08-30 21:17:53 -0700

    Swap collage to PNG version, cropped tight to remove white border

--- src/components/Collage.tsx
+++ src/components/Collage.tsx
@@ -3,7 +3,7 @@ export default function Collage() {
     <section id="collage" className="mx-auto max-w-5xl px-6 pb-28">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/collage/composite.jpg"
+        src="/collage/composite.png"
         alt="A collage of Kaitlyn Chen's work: the KNN nonprofit dashboard map, WSJ Challenges app screens, a magazine-style feature titled 'On Redefining Herness,' a dance photograph, and a whiteboard mapping obstacles to disruptive innovation"
         className="w-full"
       />
